85a74a49bbc152c8c19d90af4455b8780812c943
Marcelo <marcelo@hibejix.local>
2018-08-18 20:46:13 -0300

    Definicoes iniciais do projeto

--- Previsão do Tempo/ViewController.swift
+++ Previsão do Tempo/ViewController.swift
@@ -10,16 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelData: UILabel!
+    @IBOutlet weak var labelInformacoesCidade: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var labelTemperatura: UILabel!
+    @IBOutlet weak var labelMinMaxTemperatura: UILabel!
+    
+    let API_KEY = "9526ed8093dc32fd14a8d0941367665b";
+    let fullURL = "http://api.openweathermap.org/data/2.5/forecast?q=London,us&appid=9526ed8093dc32fd14a8d0941367665b";
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func textFieldEndEditing(_ sender: UITextField) {
+    }
 
+    @IBAction func pressedHoje(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func pressedAmanha(_ sender: UIBarButtonItem) {
+    }
+    
 }
 
